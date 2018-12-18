6321aeee39c64bec62e0c62494f20bdcfc8804cb
seethend <seetheendreddy96@gmail.com>
2018-12-19 00:22:23 +0530

    Office - page to create a new topic

--- src/app/discussions/topic/write-topic/write-topic.component.ts
+++ src/app/discussions/topic/write-topic/write-topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { TopicService } from '../topic.service';
 
 @Component({
@@ -8,8 +8,20 @@ import { TopicService } from '../topic.service';
 })
 export class WriteTopicComponent implements OnInit {
 
+    @ViewChild("topicTagsList")
+    topicTagsList: ElementRef;
+
+    topicTitleInput = '';
+    topicBodyInput = '';
     topicTagInput = '';
-    topicTagOptions = [];
+
+    isTopicTitleInputNull = false;
+    isTopicBodyInputNull = false;
+    isTopicTagInputNull = false;
+
+    allTopicTagOptions = [];
+    selectedTopicTags = [];
+
 
     constructor(private topicService: TopicService) { }
 
@@ -17,17 +29,76 @@ export class WriteTopicComponent implements OnInit {
     }
 
     showAllTopicTagsMatches() {
-        this.topicTagOptions = [];
+        this.allTopicTagOptions = [];
         if (this.topicTagInput.length > 2) {
+            this.topicTagsList.nativeElement.style.display = 'block';
             const topicTags = this.topicService.getAllTags();
             // console.log(topicTags, this.topicTag);
             for (const tag of topicTags) {
                 let tagMatches = [];
                 tagMatches = tag.match(this.topicTagInput);
                 if (tagMatches != null && tagMatches.length > 0) {
-                    this.topicTagOptions.push(tag);
+                    this.allTopicTagOptions.push(tag);
                 }
             }
+            // console.log(this.allTopicTagOptions);
+        }
+        else{
+            this.topicTagsList.nativeElement.style.display = 'none';
+        }
+    }
+
+    selectCurrentTag(topicTag){
+        if(this.selectedTopicTags.indexOf(topicTag) < 0){
+            this.selectedTopicTags.push(topicTag);
+        }
+        this.topicTagInput = '';
+        document.getElementById('topic-tags').focus();
+        this.showAllTopicTagsMatches()
+        this.checkTopicTagInputField()
+        // console.log(this.selectedTopicTags)
+    }
+
+    deleteSelectedTag(selectedCurrentTag){
+        const index = this.selectedTopicTags.indexOf(selectedCurrentTag, 0);
+        if (index > -1) {
+            this.selectedTopicTags.splice(index, 1);
+        }
+        this.checkTopicTagInputField()
+        // console.log(selectedCurrentTag, this.selectedTopicTags)
+    }
+
+    checkTopicTagInputField(){
+        if(this.selectedTopicTags.length > 0){
+            this.isTopicTagInputNull = false;
+        }
+        else{
+            this.isTopicTagInputNull = true;
+        }
+    }
+
+    saveTopic(){
+        if(this.topicTitleInput.length != null && this.topicTitleInput.length > 0){
+            this.isTopicTitleInputNull = false;
+
+            if(this.topicBodyInput.length != null && this.topicBodyInput.length > 0){
+                this.isTopicBodyInputNull = false;
+
+                if(this.selectedTopicTags.length > 0){
+                    this.isTopicTagInputNull = false;
+                    
+                    console.log(this.topicTitleInput, this.topicBodyInput, this.selectedTopicTags)
+                }
+                else{
+                    this.isTopicTagInputNull = true;
+                }           
+            }
+            else{
+                this.isTopicBodyInputNull = true;
+            }         
+        }
+        else{
+            this.isTopicTitleInputNull = true;
         }
     }
 
