120cbd1dc2a31fa3099b4364511712935176d522
seethend <seethendreddy96@gmail.com>
2019-02-09 02:31:57 +0530

    Search module working

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,3 +1,6 @@
+import { Search } from './search.model';
+import { ActivatedRoute, Params } from '@angular/router';
+import { SearchService } from './search.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  searchString = '';
+  search: Search;
+  isSearchNotNull = false;
+
+  isUsersNotNull = false;
+  isPostsNotNull = false;
+  isTopicsNotNull = false;
+
+  constructor(private searchService: SearchService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.route.params.subscribe (
+      (params: Params) => {
+        this.searchString = params['searchString'];
+        this.searchService.getAllMatchedModels(this.searchString);
+        this.searchService.searchLoaded.subscribe(
+          (localSearch: Search) => {
+            this.search = localSearch;
+            if (this.search != null) {
+              this.isSearchNotNull = true;
+            } else {
+              this.isSearchNotNull = false;
+            }
+            this.checkNulls();
+          },
+          error => {
+            console.log('Search Results fetched successfully...But something wrong happened');
+          }
+        );
+      }
+    );
+
+  }
+  checkNulls(): any {
+
+    if (this.search.users != null && this.search.users.length > 0) {
+      this.isUsersNotNull = true;
+    } else {
+      this.isUsersNotNull = false;
+    }
+
+    if (this.search.posts != null && this.search.posts.length > 0) {
+      this.isPostsNotNull = true;
+    } else {
+      this.isPostsNotNull = false;
+    }
+
+    if (this.search.topics != null && this.search.topics.length > 0) {
+      this.isTopicsNotNull = true;
+    } else {
+      this.isTopicsNotNull = false;
+    }
   }
 
 }
