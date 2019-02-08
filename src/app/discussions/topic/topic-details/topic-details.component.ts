6d0dd299962add0ed5ccae56e632c6118b770f9b
seethend <seethendreddy96@gmail.com>
2019-02-09 02:29:09 +0530

    Check for topic loaded added

--- src/app/discussions/topic/topic-details/topic-details.component.ts
+++ src/app/discussions/topic/topic-details/topic-details.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticateService } from './../../../authenticate/authenticate.service';
 import { Component, OnInit } from '@angular/core';
 import {TopicService} from '../topic.service';
 import {ActivatedRoute, Params} from '@angular/router';
@@ -13,14 +14,31 @@ export class TopicDetailsComponent implements OnInit {
   topic: Topic;
   topicId: string;
 
-  constructor(private topicService: TopicService, private route: ActivatedRoute) { }
+  isTopicLoaded = false;
+
+  constructor(private topicService: TopicService, private route: ActivatedRoute, private authService: AuthenticateService) { }
 
   ngOnInit() {
     // Fires everytime id changes in url and gets new topic based on Id from topicService
     this.route.params.subscribe(
       (params: Params) => {
+        this.isTopicLoaded = false;
         this.topicId = params['id'];
         this.topic = this.topicService.getTopicById(this.topicId);
+        if (this.topic == null) {
+          this.topicService.getTopicFromAPI(this.topicId).subscribe(
+            (localTopic: Topic) => {
+              this.topic = localTopic;
+              this.isTopicLoaded = true;
+            },
+            error => {
+              this.isTopicLoaded = false;
+              this.authService.logout();
+            }
+          );
+        } else {
+          this.isTopicLoaded = true;
+        }
       }
     );
   }
